0d1ad318ca9d7bb6dc9c0d3a370cf1e26e811d73
Tengis Batsaikhan <tengee0411@gmail.com>
2017-06-28 22:54:45 +1000

    added main function

---
+++ index.js
@@ -0,0 +1,18 @@
+const http = require('http');
+const https = require('https');
+const drawInIterm = require('iterm2-image');
+const code = process.argv[2];
+
+if (Object.keys(http.STATUS_CODES).indexOf(code) === -1) {
+  console.log(`httpcat: ${code}: No such http code exists`);
+  process.exit();
+}
+
+https.get(`https://http.cat/${code}`, (res) => {
+  if (res.statusCode === 200) {
+    drawInIterm(res, function (err) {
+      if (err) { throw err; }
+      process.exit(0);
+    });
+  }
+});
